c96db46e9f9891a097e17027d7b0d86d2a8a9a68
justin <justin@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2002-08-01 17:18:12 +0000

    @19464 	       A fix.  BTW, as Vadim pointed out, there is already a map like 	       this, SequentialMap.  I'm not getting rid of this yet, since 	       it may be faster for reads on large sets.  SequentialMap uses 	       an ArrayList internally, and OrderedMap uses a TreeMap with a 	       special Comparator that tracks the order in which puts were 	       made.

--- archive/core-platform/src/com/arsdigita/util/OrderedMap.java
+++ archive/core-platform/src/com/arsdigita/util/OrderedMap.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2001 ArsDigita Corporation. All Rights Reserved.
  *
- * The contents of this file are subject to the ArsDigita Public 
+ * The contents of this file are subject to the ArsDigita Public
  * License (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of
  * the License at http://www.arsdigita.com/ADPL.txt
@@ -28,10 +28,10 @@ import org.apache.log4j.Category;
  * @author Justin Ross &lt;<a href="mailto:jross@redhat.com">jross@redhat.com</a>&gt;
  */
 public class OrderedMap extends TreeMap {
-    public static final String versionId = 
-        "$Id: //core-platform/dev/src/com/arsdigita/util/OrderedMap.java#1 $" +
+    public static final String versionId =
+        "$Id: //core-platform/dev/src/com/arsdigita/util/OrderedMap.java#2 $" +
         "$Author: justin $" +
-        "$DateTime: 2002/08/01 14:48:43 $";
+        "$DateTime: 2002/08/01 17:18:12 $";
 
     private static Category s_log = Category.getInstance
         (OrderedMap.class.getName());
@@ -53,7 +53,7 @@ public class OrderedMap extends TreeMap {
             s_log.debug("Adding a new map entry: " + key + " => " + value);
         }
 
-        m_comparator.keep(value);
+        m_comparator.keep(key);
 
         return super.put(key, value);
     }
@@ -62,16 +62,18 @@ public class OrderedMap extends TreeMap {
 class OrderedComparator implements Comparator {
     private HashMap m_sortKeyMap = new HashMap();
     private long m_currSortKey = 0;
-    
+
     public int compare(Object o1, Object o2) {
-        long sk1 = ((Long) m_sortKeyMap.get(o1)).longValue();
-        long sk2 = ((Long) m_sortKeyMap.get(o2)).longValue();
-        
-        return (int) (sk1 - sk2);
+        Long sk1 = (Long) m_sortKeyMap.get(o1);
+        Long sk2 = (Long) m_sortKeyMap.get(o2);
+
+        if (sk1 == null) return 1;
+        if (sk2 == null) return -1;
+
+        return (int) (sk1.longValue() - sk2.longValue());
     }
-    
-    public void keep(Object value) {
-        m_sortKeyMap.put(value, new Long(m_currSortKey++));
+
+    public void keep(Object key) {
+        m_sortKeyMap.put(key, new Long(m_currSortKey++));
     }
 }
-
