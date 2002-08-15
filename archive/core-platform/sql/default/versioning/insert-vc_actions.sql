7412fbd0b64e8dce195662199374b8f232e1a78a
randyg <randyg@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2002-08-15 17:08:42 +0000

    @19984 	more mdsql and fixed the "insert" for vc_actions so that the tests pass

--- archive/core-platform/sql/default/versioning/insert-vc_actions.sql
+++ archive/core-platform/sql/default/versioning/insert-vc_actions.sql
@@ -2,3 +2,29 @@
   values (
     'create', 'Create a new value for the attribute'
   );
+
+ insert into vc_actions (action, description)
+  values (
+    'update', 'Update the attribute''s value (for single-valued
+attributes)'
+  );
+
+  insert into vc_actions (action, description)
+  values (
+    'add', 'Add a value for a multi-valued attribute'
+  );
+
+  insert into vc_actions (action, description)
+  values (
+    'remove', 'Remove a value from a multi-valued attribute'
+  );
+
+  insert into vc_actions (action, description)
+  values (
+    'create_content', 'Create new content for the object'
+  );
+
+  insert into vc_actions (action, description)
+  values (
+    'update_content', 'Modify existing content for the object'
+  );
