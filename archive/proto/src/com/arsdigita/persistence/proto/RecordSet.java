06524e66caab8614526529af728b94c4ba46e813
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-01-17 12:53:32 +0000

    @25932 	Fixed a bug in data loading that allowed OIDs with null values 	to be loaded into the session.

--- archive/proto/src/com/arsdigita/persistence/proto/RecordSet.java
+++ archive/proto/src/com/arsdigita/persistence/proto/RecordSet.java
@@ -10,12 +10,12 @@ import java.util.*;
  * RecordSet
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #3 $ $Date: 2003/01/15 $
+ * @version $Revision: #4 $ $Date: 2003/01/17 $
  **/
 
 public abstract class RecordSet {
 
-    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/persistence/proto/RecordSet.java#3 $ by $Author: rhs $, $DateTime: 2003/01/15 16:58:00 $";
+    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/persistence/proto/RecordSet.java#4 $ by $Author: rhs $, $DateTime: 2003/01/17 12:53:32 $";
 
     private Signature m_signature;
 
@@ -42,19 +42,26 @@ public abstract class RecordSet {
             Path p = (Path) it.next();
             Path parent = p.getParent();
             if (type.isKey(p)) {
+                Object value = get(p);
                 OID oid;
                 if (oids.containsKey(parent)) {
                     oid = (OID) oids.get(parent);
                 } else {
-                    if (parent == null) {
-                        oid = new OID(type);
+                    if (value == null) {
+                        oid = null;
                     } else {
-                        oid = new OID(type.getType(parent));
+                        if (parent == null) {
+                            oid = new OID(type);
+                        } else {
+                            oid = new OID(type.getType(parent));
+                        }
                     }
                     oids.put(parent, oid);
                 }
 
-                oid.set(p.getName(), get(p));
+                if (value != null) {
+                    oid.set(p.getName(), value);
+                }
             }
         }
 
@@ -64,17 +71,25 @@ public abstract class RecordSet {
             Path parent = p.getParent();
 
             OID oid = (OID) oids.get(parent);
-            if (!oid.isInitialized()) {
-                throw new IllegalStateException
-                    ("Query does not fetch OID values.");
-            }
 
-            Property prop = oid.getObjectType().getProperty(p.getName());
-
-            if (type.isKey(p)) {
-                ssn.load(oid, prop, oid.get(p.getName()));
+            if (oid == null) {
+                Object value = get(p);
+                if (value != null) {
+                    throw new IllegalStateException
+                        ("key was null but value isn't");
+                }
             } else {
-                ssn.load(oid, prop, get(p));
+                if (!oid.isInitialized()) {
+                    throw new IllegalStateException
+                        ("Query does not fetch OID values.");
+                }
+                Property prop = oid.getObjectType().getProperty(p.getName());
+
+                if (type.isKey(p)) {
+                    ssn.load(oid, prop, oid.get(p.getName()));
+                } else {
+                    ssn.load(oid, prop, get(p));
+                }
             }
         }
 
