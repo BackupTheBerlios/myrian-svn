1c51a5d230b269c157045a64ccc6d93318516aba
vadim <vadim@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-04-04 16:27:43 +0000

    @29523 	typo correction

--- archive/proto/src/com/arsdigita/persistence/proto/NotFetchedException.java
+++ archive/proto/src/com/arsdigita/persistence/proto/NotFetchedException.java
@@ -6,17 +6,17 @@ import com.arsdigita.persistence.proto.common.*;
  * NotFetchedException
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #1 $ $Date: 2003/04/04 $
+ * @version $Revision: #2 $ $Date: 2003/04/04 $
  **/
 
 public class NotFetchedException extends CursorException {
 
-    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/persistence/proto/NotFetchedException.java#1 $ by $Author: rhs $, $DateTime: 2003/04/04 15:25:34 $";
+    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/persistence/proto/NotFetchedException.java#2 $ by $Author: vadim $, $DateTime: 2003/04/04 16:27:43 $";
 
     private Path m_path;
 
     public NotFetchedException(Cursor c, Path p) {
-	super(c, "cursor does not not fetch path: " + p);
+	super(c, "cursor does not fetch path: " + p);
 	m_path = p;
     }
 
