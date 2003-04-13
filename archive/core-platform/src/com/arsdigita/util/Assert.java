ff935967c57a9d29d632dcaec5e01e8f7460da82
justin <justin@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-04-13 00:26:01 +0000

    @29769 	       Cleans up the assertion utility.  Specifically:
    	       - Makes it possible to optimize away expensive assertions.  Lays ground
    	         for making the assert-enabled boolean a runtime property, if we
    	         decide we want that.
    	
    	         Some may not be aware that, up till now, we have *always* executed
    	         assertion logic.  For the longest time, ASSERT_ON has been a static
    	         final on Assert, and it's set true.
    	
    	         (When you see assertions that do any work in their arguments, take
    	         that chance to wrap them in an if (Assert.isEnabled()) {} block.)
    	
    	       - For now, assertions are defaulted to on as they have been
    	         historically.  The default will change.  Meanwhile, you can disable
    	         assertions with this enterprise.init block:
    	
    	           com.arsdigita.util.Initializer {
    	               isAssertEnabled = false;
    	           }
    	
    	       - For brevity, offers shorter variants of static assert methods.  These
    	         use the name of the class "Assert" as part of the API's pseudo
    	         english.  The old, longer variants are deprecated.
    	
    	       - The new methods throw Errors, not Exceptions.  This is in line with
    	         what java 1.4 assertions do, and it makes more sense anyway.  If you
    	         catch assertions, you should be flogged.
    	
    	       - Leaves off methods that don't result in a helpful failure message.
    	
    	       - I opted not to create equal and unequal variants for every Java
    	         primitive type.  In such cases, Assert.truth(condition, message) will
    	         suffice.
    	
    	       - To achieve some consistency and precision in reporting failures,
    	         assertNotNull(obj, str) becomes exists(obj, clacc).  We lose the
    	         instance name (assuming the developer was good enough to spell it out
    	         in the message string with the old API), but we gain on consistency
    	         of error reporting, and it's more mechanical to write.
    	
    	         Of course, the problem scenario for this is a method that takes a
    	         bunch of strings.  What's the best thing to do?  I don't think it's
    	         especially worth worrying about.
    	
    	       - Avoids delegation in assert methods so the stack traces have a little
    	         less noise.
    	
    	       - The API changes are all opt-in and don't impact existing code.
    	
    	       - Notes in the Javadoc of some methods and classes that they should be
    	         final and will be made so in the future, so don't subclass them.
    	
    	       - To compensate for some of the bad exception handling we have and
    	         avoid the resulting hidden failures, assertions emit a one-line log
    	         message before they throw an exception or error.
    	
    	       - The assertion methods do their work regardless of whether assertions
    	         are enabled, since the actual assertion checks are as cheap as
    	         determining whether assertions are enabled.  If you really want to
    	         save work, you need to use isEnabled() blocks.
    	
    	       Summary of the new assertion methods:
    	
    	         if (Assert.isEnabled()) {
    	             Assert.truth(condition, message)
    	             Assert.falsity(condition, message)
    	             Assert.exists(object, clacc)
    	             Assert.locked(lockable)
    	             Assert.unlocked(lockable)
    	             Assert.equal(object1, object2)
    	             Assert.unequal(object1, object2)
    	         }
    	
    	         UtilConfig.isAssertEnabled() (Assert.isEnabled() calls this)
    	         UtilConfig.setAssertEnabled(bool)
    	
    	       Methods deprecated with no new equivalents:
    	
    	         assertTrue(condition)
    	
    	           I didn't put in an truth(condition) because I think it's better if
    	           we take the time to report what went wrong beyond "assertion
    	           failed".  However, if someone complains enough, I'll put the
    	           message-less assertions back in.
    	
    	         assertNotEmpty(str)
    	
    	           It's only used in three places in all of core and CMS.

--- archive/core-platform/src/com/arsdigita/util/Assert.java
+++ archive/core-platform/src/com/arsdigita/util/Assert.java
@@ -20,40 +20,213 @@ import org.apache.log4j.Logger;
 /**
  * Utility functions for assertions.
  *
- * The static methods in this class provide a standard way of asserting
- * certain conditions.
+ * The static methods in this class provide a standard way of
+ * asserting certain conditions.
+ *
+ * Though it is not right now, this class <em>should</em> be final.
+ * Do not subclass it.  In a future revision of this software, this
+ * class will be made final.
  *
  * @author David Lutterkort
  * @author Uday Mathur
- * @version $Id: //core-platform/dev/src/com/arsdigita/util/Assert.java#9 $
- *
+ * @author Justin Ross &lt;jross@redhat.com&gt;
+ * @version $Id: //core-platform/dev/src/com/arsdigita/util/Assert.java#10 $
  */
 public class Assert {
     public static final String versionId =
-        "$Id: //core-platform/dev/src/com/arsdigita/util/Assert.java#9 $" +
+        "$Id: //core-platform/dev/src/com/arsdigita/util/Assert.java#10 $" +
         "$Author: justin $" +
-        "$DateTime: 2003/02/20 19:30:34 $";
+        "$DateTime: 2003/04/13 00:26:01 $";
 
     private static final Logger s_log = Logger.getLogger
         (Assert.class);
 
+    /**
+     * Tells whether asserts are turned on.  Use this to wrap code
+     * that should be optimized away if assertions are disabled.
+     */
+    public static final boolean isEnabled() {
+        // Making this a little less costly will be good. XXX
+
+        return Util.getConfig().isAssertEnabled();
+    }
+
+    /**
+     * Asserts that an arbitrary condition is true and throws an
+     * error with message <code>message</code> if the condition is
+     * false.
+     *
+     * @param condition The condition asserted
+     * @param message An error message
+     * @throws java.lang.Error if the condition is false
+     */
+    public static final void truth(final boolean condition,
+                                   final String message) {
+        if (!condition) {
+            s_log.error(message);
+
+            throw new Error(message);
+        }
+    }
+
+    /**
+     * Asserts that an arbitrary condition is false and throws an
+     * error if the condition is true.
+     *
+     * @param condition The condition asserted
+     * @param message An error message
+     * @throws java.lang.Error if the condition is false
+     */
+    public static final void falsity(final boolean condition,
+                                     final String message) {
+        if (condition) {
+            s_log.error(message);
+
+            throw new Error(message);
+        }
+    }
+
+    /**
+     * Asserts that an object is not null.
+     *
+     * @param object The object that must not be null
+     * @param clacc The <code>Class</code> of parameter
+     * <code>object</code>
+     * @throws java.lang.Error if the object is null
+     */
+    public static final void exists(final Object object,
+                                    final Class clacc) {
+        if (object == null) {
+            final String message = clacc.getName() + " is null";
+
+            s_log.error(message);
+
+            throw new Error(message);
+        }
+    }
+
+    /**
+     * Verifies that <code>Lockable</code> is locked and throws an
+     * error if it is not.
+     *
+     * @param lockable The object that must be locked
+     * @see com.arsdigita.util.Lockable
+     */
+    public static final void locked(final Lockable lockable) {
+        if (lockable != null && !lockable.isLocked()) {
+            final String message = lockable + " is not locked";
+
+            s_log.error(message);
+
+            throw new Error(message);
+        }
+    }
+
+    /**
+     * Verifies that <code>lockable</code> is <em>not</em> locked and
+     * throws an error if it is.
+     *
+     * @param lockable The object that must not be locked
+     * @see com.arsdigita.util.Lockable
+     */
+    public static final void unlocked(final Lockable lockable) {
+        if (lockable != null && lockable.isLocked()) {
+            final String message = lockable + " is locked";
+
+            s_log.error(message);
+
+            throw new Error(message);
+        }
+    }
+
+    /**
+     * Verifies that two values are equal (according to their equals
+     * method, unless <code>value1</code> is null, then according to
+     * <code>==</code>).
+     *
+     * @param value1 The first value to be compared
+     * @param value2 The second
+     * @throws java.lang.Error if the arguments are unequal
+     */
+    public static final void equal(final Object value1,
+                                   final Object value2) {
+        if (value1 == null) {
+            if (value1 != value2) {
+                final String message = value1 + " does not equal " + value2;
+
+                s_log.error(message);
+
+                throw new Error(message);
+            }
+        } else {
+            if (!value1.equals(value2)) {
+                final String message = value1 + " does not equal " + value2;
+
+                s_log.error(message);
+
+                throw new Error(message);
+            }
+        }
+    }
+
+    /**
+     * Verifies that two values are not equal (according to their
+     * equals method, unless <code>value1</code> is null, then
+     * according to <code>==</code>).
+     *
+     * @param value1 The first value to be compared
+     * @param value2 The second
+     * @throws java.lang.Error if the arguments are unequal
+     */
+    public static final void unequal(final Object value1,
+                                     final Object value2) {
+        if (value1 == null) {
+            if (value1 == value2) {
+                final String message = value1 + " equals " + value2;
+
+                s_log.error(message);
+
+                throw new Error(message);
+            }
+        } else {
+            if (value1.equals(value2)) {
+                final String message = value1 + " equals " + value2;
+
+                s_log.error(message);
+
+                throw new Error(message);
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                                                                       //
+    // The methods below are all deprecated.                                 //
+    //                                                                       //
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @deprecated in favor of {@link #isEnabled()}
+     */
     public static final boolean ASSERT_ON = true;
 
     /**
      * Indicates state of the ASSERT_ON flag.
      *
-     * @deprecated Use {#isAssertEnabled()} instead.
+     * @deprecated Use {@link #isEnabled()} instead
      */
     public static final boolean isAssertOn() {
-        return isAssertEnabled();
+        return isEnabled();
     }
 
     /**
      * Tells whether asserts are turned on.  Use this to wrap code
      * that should be optimized away if asserts are disabled.
+     *
+     * @deprecated Use {@link #isEnabled()} instead
      */
     public static final boolean isAssertEnabled() {
-        return ASSERT_ON;
+        return isEnabled();
     }
 
     /**
@@ -62,6 +235,7 @@ public class Assert {
      *
      * @param cond condition to assert
      * @throws java.lang.IllegalStateException condition was false
+     * @deprecated Use {@link #isTrue(boolean)} instead
      */
     public static final void assertTrue(boolean cond) {
         assertTrue(cond, "");
@@ -75,16 +249,21 @@ public class Assert {
      * @param cond condition to assert
      * @param msg failure message
      * @throws java.lang.IllegalStateException condition was false
+     * @deprecated Use {@link #isTrue(boolean,String)} instead
      */
     public static final void assertTrue(boolean cond, String msg) {
-        if (ASSERT_ON && !cond) {
-            throw new IllegalStateException("Assertion failed: " + msg);
+        if (!cond) {
+            s_log.error(msg);
+
+            throw new IllegalStateException(msg);
         }
     }
 
     /**
      * Verify that a parameter is not null and throw a runtime
      * exception if so.
+     *
+     * @deprecated Use {@link #isNotNull(Object,Class)} instead
      */
     public static final void assertNotNull(Object o) {
         assertNotNull(o, "");
@@ -93,9 +272,11 @@ public class Assert {
     /**
      * Verify that a parameter is not null and throw a runtime
      * exception if so.
+     *
+     * @deprecated Use {@link #isNotNull(Object,Class) instead
      */
     public static final void assertNotNull(Object o, String label) {
-        if (ASSERT_ON) {
+        if (isEnabled()) {
             assertTrue(o != null, "Value of " + label + " is null.");
         }
     }
@@ -104,6 +285,8 @@ public class Assert {
      * Verify that a string is not empty and throw a runtime exception
      * if so.  A parameter is considered empty if it is null, or if it
      * does not contain any characters that are non-whitespace.
+     *
+     * @deprecated with no replacement
      */
     public static final void assertNotEmpty(String s) {
         assertNotEmpty(s, "");
@@ -113,9 +296,11 @@ public class Assert {
      * Verify that a string is not empty and throw a runtime exception
      * if so.  A parameter is considered empty if it is null, or if it
      * does not contain any characters that are non-whitespace.
+     *
+     * @deprecated with no replacement
      */
     public static final void assertNotEmpty(String s, String label) {
-        if (ASSERT_ON) {
+        if (isEnabled()) {
             assertTrue(s != null && s.trim().length() > 0,
                        "Value of " + label + " is empty.");
         }
@@ -128,6 +313,7 @@ public class Assert {
      * @param expected Expected value.
      * @param actual Actual value.
      * @throws java.lang.IllegalStateException condition was false
+     * @deprecated Use {@link equal(Object,Object) instead
      */
     public static final void assertEquals(Object expected, Object actual) {
         assertEquals(expected, actual, "expected", "actual");
@@ -146,7 +332,7 @@ public class Assert {
     public static final void assertEquals(Object expected, Object actual,
                                           String expectedLabel,
                                           String actualLabel) {
-        if (ASSERT_ON) {
+        if (isEnabled()) {
             if (expected == null) {
                 assertTrue(expected == actual,
                            "Values not equal, " +
@@ -167,6 +353,7 @@ public class Assert {
      * @param expected Expected value.
      * @param actual Actual value.
      * @throws java.lang.IllegalStateException condition was false
+     * @deprecated Use {@link #isTrue(boolean)} instead
      */
     public static final void assertEquals(int expected, int actual) {
         assertEquals(expected, actual, "expected", "actual");
@@ -180,11 +367,12 @@ public class Assert {
      * @param expectedLabel Label for first (generally expected) value.
      * @param actualLabel Label for second (generally actual) value.
      * @throws java.lang.IllegalStateException condition was false
+     * @deprecated Use {@link #isTrue(boolean)} instead
      */
     public static final void assertEquals(int expected, int actual,
                                           String expectedLabel,
                                           String actualLabel) {
-        if (ASSERT_ON) {
+        if (isEnabled()) {
             assertTrue(expected == actual,
                        "Values not equal, " +
                        expectedLabel + " '" + expected + "', " +
@@ -195,25 +383,38 @@ public class Assert {
     /**
      * Verify that the model is locked and throw a runtime exception
      * if it is not locked.
+     *
+     * @deprecated Use {@link #locked(Lockable)} instead
      */
     public static void assertLocked(Lockable l) {
-        assertTrue(l.isLocked(),
-                   "Illegal access to an unlocked " + l.getClass().getName());
+        if (isEnabled()) {
+            assertTrue(l.isLocked(),
+                       "Illegal access to an unlocked " +
+                       l.getClass().getName());
+        }
     }
 
     /**
      * Verify that the model is locked and throw a runtime exception
      * if it is locked.
+     *
+     * @deprecated Use {@link #unlocked(Lockable)} instead
      */
     public static void assertNotLocked(Lockable l) {
-        assertTrue (!l.isLocked(),
-                    "Illegal access to a locked " + l.getClass().getName());
+        if (isEnabled()) {
+            assertTrue (!l.isLocked(),
+                        "Illegal access to a locked " +
+                        l.getClass().getName());
+        }
     }
 
     /**
      * This is the equivalent of assertTrue(false, msg).
      *
      * @param msg A string describing the condition of failure.
+     * @throws java.lang.Error
+     * @deprecated because it's just as simple to throw an
+     * <code>Error</code> or <code>Exception</code>
      */
     public static void fail(String msg) {
         assertTrue(false, msg);
@@ -221,6 +422,9 @@ public class Assert {
 
     /**
      * This is the equivalent of assertTrue(false).
+     *
+     * @deprecated because it's just as simple to throw an
+     * <code>Error</code> or <code>Exception</code>
      */
     public static void fail() {
         assertTrue(false);
---
+++ archive/core-platform/src/com/arsdigita/util/UtilConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2001, 2002 Red Hat Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the CCM Public
+ * License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of
+ * the License at http://www.redhat.com/licenses/ccmpl.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ */
+
+package com.arsdigita.util;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Justin Ross
+ * @see com.arsdigita.util.Util
+ */
+final class UtilConfig extends Record {
+    public static final String versionId =
+        "$Id: //core-platform/dev/src/com/arsdigita/util/UtilConfig.java#1 $" +
+        "$Author: justin $" +
+        "$DateTime: 2003/04/13 00:26:01 $";
+
+    private static final Logger s_log = Logger.getLogger(UtilConfig.class);
+
+    private boolean m_isAssertEnabled = true;
+
+    private static final String[] s_fields = new String[] {
+        "AssertEnabled"
+    };
+
+    UtilConfig() {
+        super(UtilConfig.class, s_log, s_fields);
+    }
+
+    public final boolean isAssertEnabled() {
+        accessed("AssertEnabled");
+
+        return m_isAssertEnabled;
+    }
+
+    final void setAssertEnabled(boolean enabled) {
+        m_isAssertEnabled = enabled;
+
+        mutated("AssertEnabled");
+    }
+}
