befc8b0dbca861ac84bcf5e5864e9e4ddc75fb89
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-04-25 15:49:01 +0000

    @30255 	Integrated change 30184 and 30185 onto the proto branch. SDM 229096.

--- archive/proto/src/com/arsdigita/persistence/InFilter.java
+++ archive/proto/src/com/arsdigita/persistence/InFilter.java
@@ -5,25 +5,32 @@ import com.arsdigita.persistence.proto.metadata.*;
 
 import java.util.*;
 
+import org.apache.log4j.Logger;
+
 /**
  * InFilter
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #2 $ $Date: 2003/04/23 $
+ * @version $Revision: #3 $ $Date: 2003/04/25 $
  **/
 
 class InFilter extends FilterImpl implements Filter {
 
-    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/persistence/InFilter.java#2 $ by $Author: ashah $, $DateTime: 2003/04/23 10:50:52 $";
+    private static Logger s_log = Logger.getLogger(InFilter.class);
+
+    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/persistence/InFilter.java#3 $ by $Author: rhs $, $DateTime: 2003/04/25 15:49:01 $";
 
     private String m_prop;
     private String m_subProp;
     private String m_query;
 
     InFilter(String property, String subqueryProperty, String query) {
-	m_prop = property;
-	m_subProp = subqueryProperty;
-	m_query = query;
+        m_prop = property;
+        m_subProp = subqueryProperty;
+        m_query = query;
+        if (s_log.isDebugEnabled()) {
+            s_log.debug("InFilter: " + property + " - " + subqueryProperty + " - " + query);
+        }
     }
 
     private SQLBlock getBlock(String query) {
@@ -34,7 +41,6 @@ class InFilter extends FilterImpl implements Filter {
 
     public String getConditions() {
 	SQLBlock block = getBlock(m_query);
-
 	Path subProp;
 	if (m_subProp == null) {
 	    Iterator paths = block.getPaths().iterator();
@@ -57,9 +63,10 @@ class InFilter extends FilterImpl implements Filter {
 	    throw new MetadataException(block, "no such path: " + subProp);
 	}
 
-	return "exists ( select \"" + subcol.getPath() +
-	    "\" from (" + m_query + ") \"insub\" where \"insub\".\"" +
-	    subcol.getPath() + "\" = " + m_prop + ")";
+	return "exists ( select \"subquery_id\" from (select \"" +
+            subcol.getPath() + "\" as \"subquery_id\" from (" +
+            m_query + ") \"insub1\" ) \"insub2\" where " +
+            "\"insub2\".\"subquery_id\" = " + m_prop + ")";
     }
 
 }
