14677d3cfef427f37f5b59faf1f133d1d1a8c419
ashah <ashah@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-04-30 09:42:47 +0000

    @30434 	use DomainObjectObserver for HierarchyDenormalization

--- archive/proto/src/com/arsdigita/util/HierarchyDenormalization.java
+++ archive/proto/src/com/arsdigita/util/HierarchyDenormalization.java
@@ -16,9 +16,12 @@
 package com.arsdigita.util;
 
 import com.arsdigita.persistence.DataCollection;
+import com.arsdigita.persistence.DataObject;
 import com.arsdigita.persistence.DataOperation;
 import com.arsdigita.persistence.SessionManager;
 import com.arsdigita.domain.DomainObject;
+import com.arsdigita.domain.DomainObjectObserver;
+import com.arsdigita.domain.ObservableDomainObject;
 
 import org.apache.log4j.Logger;
 
@@ -31,116 +34,103 @@ import org.apache.log4j.Logger;
  * </p>
  *
  * @author <a href="mailto:randyg@alum.mit.edu">Randy Graebner</a>
- * @version $Revision: #2 $ $Date: 2003/04/15 $
+ * @version $Revision: #3 $ $Date: 2003/04/30 $
  */
 public abstract class HierarchyDenormalization {
 
-    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/util/HierarchyDenormalization.java#2 $ by $Author: ashah $, $DateTime: 2003/04/15 10:38:49 $";
+    public final static String versionId = "$Id: //core-platform/proto/src/com/arsdigita/util/HierarchyDenormalization.java#3 $ by $Author: ashah $, $DateTime: 2003/04/30 09:42:47 $";
 
-    private final static Logger s_log = 
+    private final static Logger s_log =
         Logger.getLogger(HierarchyDenormalization.class);
 
     private String m_attributeName;
     private String m_id;
-    private DomainObject m_domainObject;
     private String m_operationName;
     private boolean m_isModified = false;
     private String m_oldAttributeValue;
+    private String m_newAttributeValue;
 
 
-    public HierarchyDenormalization(String operationName, DomainObject object,
+    public HierarchyDenormalization(String operationName,
+                                    ObservableDomainObject object,
                                     String attributeName) {
         this(operationName, object, attributeName, "id");
     }
 
     // id must be a property in the OID
-    public HierarchyDenormalization(String operationName, DomainObject object,
+    public HierarchyDenormalization(String operationName,
+                                    ObservableDomainObject object,
                                     String attributeName, String id) {
         m_id = id;
         m_operationName = operationName;
-        m_domainObject = object;
         m_attributeName = attributeName;
+        object.addObserver(new Observer());
     }
 
+    private class Observer implements DomainObjectObserver {
 
-    /**
-     *  This should be called by a domain object before calling super.save()
-     *  so that the correct variables can be set in preparation for the
-     *  {@link #afterSave()} call.
-     */
-    public void beforeSave() {
-        m_oldAttributeValue = null;
-        m_isModified = m_domainObject.isPropertyModified(m_attributeName);
-        boolean wasNew = m_domainObject.isNew();
-
-        if (s_log.isDebugEnabled()) {
-            s_log.debug("Before save: oid:" + m_domainObject.getOID() +
-                        " isModified:" + m_isModified +
-                        " wasNew:" + wasNew);
-        }
-
-        // if the url has been modified, we need the old url
-        // if it is modified and new then this is the first url so
-        // the "old" url is the "new" url
-        if (m_isModified) {
-            if (wasNew) {
-                m_oldAttributeValue = getAttributeValue();
-            } else {
-                DataCollection collection =
-                    SessionManager.getSession().retrieve
-                    (m_domainObject.getObjectType().getQualifiedName());
-                collection.addEqualsFilter(m_id,
-                                           m_domainObject.getOID().get(m_id));
-                if (collection.next()) {
-                    m_oldAttributeValue = (String)collection.get
-                        (m_attributeName);
+        public void set(DomainObject dobj, String name,
+                        Object old_value, Object new_value) {
+            if (name.equals(m_attributeName)) {
+                if (!m_isModified) {
+                    m_oldAttributeValue = (String) old_value;
+                    m_newAttributeValue = (String) new_value;
+                    m_isModified = true;
+                } else {
+                    m_newAttributeValue = (String) new_value;
                 }
-                collection.close();
-            }
-            if (s_log.isDebugEnabled()) {
-                s_log.debug("Old value is " + m_oldAttributeValue);
-            }
-
-            if (m_oldAttributeValue == null) {
-                throw new IllegalStateException();
             }
         }
-    }
 
+        public void add(DomainObject dobj, String name,
+                        DataObject dataObject) { }
 
-    /**
-     *  This updates the hierarchy and maintains the hierarchy for all
-     *  of the other objects that have this object as a parent
-     */
-    public void afterSave() {
-        if (m_isModified) {
-            if (s_log.isDebugEnabled()) {
-                s_log.debug("After save: oid:" + m_domainObject.getOID() +
-                            " new value is:"+ getAttributeValue() +
-                            " old value is:" + m_oldAttributeValue);
+        public void remove(DomainObject dobj, String name,
+                           DataObject dataObject) { }
+
+        public void clear(DomainObject dobj, String name) { }
+
+        public void beforeSave(DomainObject dobj) { }
+
+        public void afterSave(DomainObject dobj) {
+            if (m_isModified) {
+                if (s_log.isDebugEnabled()) {
+                    s_log.debug("After save: oid:" + dobj.getOID() +
+                                " new value is:" + m_newAttributeValue +
+                                " old value is:" + m_oldAttributeValue);
+                }
+
+                if (m_oldAttributeValue == null) {
+                    // after save triggered by autoflush in before save
+                    m_isModified = false;
+                    return;
+                }
+
+                DataOperation operation =
+                    SessionManager.getSession().retrieveDataOperation
+                    (m_operationName);
+                operation.setParameter("id", dobj.getOID().get(m_id));
+                operation.setParameter("newPrefix", m_newAttributeValue);
+                operation.setParameter
+                    ("oldPrefixLength", new Integer
+                     (m_oldAttributeValue.length()));
+                operation.setParameter("oldPrefix", m_oldAttributeValue);
+                operation.execute();
             }
-
-            if (m_oldAttributeValue == null) {
-                // after save triggered by autoflush in before save
-                m_isModified = false;
-                return;
-            }
-            DataOperation operation =
-                SessionManager.getSession().retrieveDataOperation
-                (m_operationName);
-            operation.setParameter("id", m_domainObject.getOID().get(m_id));
-            operation.setParameter("newPrefix", getAttributeValue());
-            operation.setParameter("oldPrefixLength",
-                                   new Integer(m_oldAttributeValue.length()));
-            operation.setParameter("oldPrefix", m_oldAttributeValue);
-            operation.execute();
+            m_isModified = false;
         }
-        m_isModified = false;
+
+        public void beforeDelete(DomainObject dobj) { }
+        public void afterDelete(DomainObject dobj) { }
     }
 
+    /**
+     * @deprecated HierarchyDenormalization now uses a DomainObjectObserver
+     */
+    public void beforeSave() { }
 
     /**
-     *  This returns the value of the actual attribute
+     * @deprecated HierarchyDenormalization now uses a DomainObjectObserver
      */
-    public abstract String getAttributeValue();
+    public void afterSave() { }
 }
