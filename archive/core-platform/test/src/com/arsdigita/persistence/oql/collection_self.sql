acecb8ed46c25a30295ed336bb8ac8ffc2129278
jorris <jorris@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-05-01 16:04:49 +0000

    @30528 	   Test fixes on trunk. Some integrations from 5.2 fixes, updates to DDL.

--- archive/core-platform/test/src/com/arsdigita/persistence/oql/collection_self.sql
+++ archive/core-platform/test/src/com/arsdigita/persistence/oql/collection_self.sql
@@ -1,10 +1,10 @@
 create table collection_self (
     test_id INTEGER not null
-        constraint collectio_self_test_id_f_NvZYL
+        constraint collectio_self_test_id_f_ckdca
           references tests(test_id) on delete cascade,
     element_id INTEGER not null
-        constraint collecti_sel_elemen_id_f_C8Arl
+        constraint collecti_sel_elemen_id_f_rmprl
           references tests(test_id) on delete cascade,
-    constraint coll_sel_tes_id_ele_id_p_YAP30
+    constraint coll_sel_ele_id_tes_id_p_7m18z
       primary key(element_id, test_id)
 );
