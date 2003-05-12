1744b0708e5f4e6294083f2b5c3d10bd6198f36a
ashah <ashah@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-05-12 16:04:41 +0000

    @30892 	p4 integrate -b core-platform-proto

--- archive/proto/sql/postgres/kernel/package-permission_denormalization.sql
+++ archive/proto/sql/postgres/kernel/package-permission_denormalization.sql
@@ -11,8 +11,8 @@
 -- implied. See the License for the specific language governing
 -- rights and limitations under the License.
 --
--- $Id: //core-platform/proto/sql/postgres/kernel/package-permission_denormalization.sql#2 $
--- $DateTime: 2003/04/09 16:35:55 $
+-- $Id: //core-platform/proto/sql/postgres/kernel/package-permission_denormalization.sql#3 $
+-- $DateTime: 2003/05/12 16:04:41 $
 
 
   create or replace function permissions_add_context (
@@ -293,4 +293,29 @@
     end if;
 
     return null;
+  end;' language 'plpgsql';
+
+  create or replace function permissions_rebuild()
+  returns integer
+  as
+  '
+   declare
+    row    record;
+   begin
+    delete from object_context_map;
+    delete from granted_context_non_leaf_map;
+    delete from ungranted_context_non_leaf_map;
+    delete from object_grants;
+
+    for row in select * from object_context
+                where context_id is not null 
+    loop
+      perform permissions_add_context(row.object_id, row.context_id);
+    end loop;
+
+    for row in select * from acs_permissions 
+    loop
+      perform permissions_add_grant(row.object_id);
+    end loop;
+    return 1;
   end;' language 'plpgsql';
