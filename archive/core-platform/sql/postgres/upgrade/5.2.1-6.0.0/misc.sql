4fe9cb4ebf2b3c83286248bc4a81863f6c024d58
dennis <dennis@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-08-14 19:42:42 +0000

    @34725 	The PG dm upgrade scripts now drop all foreign key triggers from core and cms and recreate the foreign key constraints.  This helps to normalize the pg catalog metadata.  Also, some various other fixes.

--- archive/core-platform/sql/postgres/upgrade/5.2.1-6.0.0/misc.sql
+++ archive/core-platform/sql/postgres/upgrade/5.2.1-6.0.0/misc.sql
@@ -11,8 +11,8 @@
 -- implied. See the License for the specific language governing
 -- rights and limitations under the License.
 --
--- $Id: //core-platform/dev/sql/postgres/upgrade/5.2.1-6.0.0/misc.sql#2 $
--- $DateTime: 2003/08/12 19:06:12 $
+-- $Id: //core-platform/dev/sql/postgres/upgrade/5.2.1-6.0.0/misc.sql#3 $
+-- $DateTime: 2003/08/14 19:42:42 $
 
 drop function last_attr_value(varchar,integer);
 
@@ -20,10 +20,62 @@ drop table cw_process_task_map;
 
 update cw_tasks set is_active = 0 where is_active is null;
 
-commit;
-
 alter table cat_categories add url varchar (200);
 
 drop trigger acs_permissions_cascade_del_tr on acs_objects;
 drop function acs_permissions_cascade_del_fn();
+
+-- Fix Constraint Ordering
+-- NOTE: The following ddl assumes that no tables have a referential constraint against one
+--       of these tables.  This holds for core and *should* hold in general as these tables
+--       are unlikely cadidates for foreign keys.
+--------------------------------------------------------------------------------
+alter table acs_permissions drop constraint acs_per_gra_id_obj_id__p_lrweb;
+alter table acs_permissions add
+    constraint acs_per_gra_id_obj_id__p_lrweb
+      primary key(object_id, grantee_id, privilege);
+
+alter table apm_package_type_listener_map drop constraint apm_pac_typ_lis_map_li_p_6_z6o;
+alter table apm_package_type_listener_map add
+    constraint apm_pac_typ_lis_map_li_p_6_z6o
+      primary key(package_type_id, listener_id);
+
+alter table cw_task_group_assignees drop constraint task_group_assignees_pk;
+alter table cw_task_group_assignees add
+    constraint cw_tas_gro_ass_gro_id__p_0bqv_
+        primary key(group_id, task_id);
+
+alter table group_member_map drop constraint grou_mem_map_gro_id_me_p_9zo_i;
+alter table group_member_map add
+    constraint grou_mem_map_gro_id_me_p_9zo_i
+      primary key(member_id, group_id);
+
+alter table group_subgroup_map drop constraint grou_sub_map_gro_id_su_p_8caa0;
+alter table group_subgroup_map add
+    constraint grou_sub_map_gro_id_su_p_8caa0
+      primary key(subgroup_id, group_id);
+
+alter table parameterized_privileges drop constraint para_pri_bas_pri_par_k_p_a1rpb;
+alter table parameterized_privileges add
+    constraint para_pri_bas_pri_par_k_p_a1rpb
+      primary key(param_key, base_privilege);
+
+alter table party_email_map drop constraint part_ema_map_ema_add_p_p_px7u4;
+alter table party_email_map add
+    constraint part_ema_map_ema_add_p_p_px7u4
+      primary key(party_id, email_address);
+
+alter table site_nodes add
+    constraint site_node_nam_paren_id_u_a3b4a
+      unique(parent_id, name);
+
+-- These constraints changed name
+update pg_constraint set conname = 'cat_categori_catego_id_p_yeprq' where UPPER(conname) = UPPER('cat_categories_pk');
+update pg_class set relname = 'cat_categori_catego_id_p_yeprq' where relname = 'cat_categories_pk';
+
+update pg_constraint set conname = 'cw_tas_dep_dep_tas_id__p_hdzws' where UPPER(conname) = UPPER('task_dependencies_pk');
+update pg_class set relname = 'cw_tas_dep_dep_tas_id__p_hdzws' where relname = 'task_dependencies_pk';
+
+update pg_constraint set conname = 'cw_tas_use_ass_tas_id__p_vsdyq' where UPPER(conname) = UPPER('task_user_assignees_pk');
+update pg_class set relname = 'cw_tas_use_ass_tas_id__p_vsdyq' where relname = 'task_user_assignees_pk';
 
--- archive/core-platform/sql/upgrade/postgres-5.2.1-6.0.0.sql
+++ archive/core-platform/sql/upgrade/postgres-5.2.1-6.0.0.sql
@@ -11,13 +11,15 @@
 -- implied. See the License for the specific language governing
 -- rights and limitations under the License.
 --
--- $Id: //core-platform/dev/sql/upgrade/postgres-5.2.1-6.0.0.sql#8 $
--- $DateTime: 2003/08/13 18:01:33 $
+-- $Id: //core-platform/dev/sql/upgrade/postgres-5.2.1-6.0.0.sql#9 $
+-- $DateTime: 2003/08/14 19:42:42 $
 
 \echo Red Hat WAF 5.2.1 -> 6.0.0 Upgrade Script (PostgreSQL)
 
 begin;
 
+\i ../postgres/upgrade/5.2.1-6.0.0/drop-ri-triggers.sql
+\i ../postgres/upgrade/5.2.1-6.0.0/drop-unique-indexes.sql
 \i ../default/upgrade/5.2.1-6.0.0/drop-search-test.sql
 \i ../default/upgrade/5.2.1-6.0.0/sequence-vcx_id_seq.sql
 \i ../default/upgrade/5.2.1-6.0.0/sequence-vcx_txns_id_seq.sql
