b968f406f66c13b7e32c0f3bb6000fc59f6da123
justin <justin@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-09-04 23:40:23 +0000

    @35708 	       Removes the caching initializer.
    	       Changes CacheTable to get its configuration from Java properties.
    	
    	       Introduces a SystemProperties class so I can use parameters to get
    	       values from Java system properties.
    	
    	       Adds a missing constructor to IntegerParameter.

--- archive/packaging/src/com/arsdigita/util/parameter/IntegerParameter.java
+++ archive/packaging/src/com/arsdigita/util/parameter/IntegerParameter.java
@@ -8,13 +8,13 @@ import org.apache.commons.beanutils.converters.*;
  * Subject to change.
  *
  * @author Justin Ross &lt;jross@redhat.com&gt;
- * @version $Id: //core-platform/test-packaging/src/com/arsdigita/util/parameter/IntegerParameter.java#2 $
+ * @version $Id: //core-platform/test-packaging/src/com/arsdigita/util/parameter/IntegerParameter.java#3 $
  */
 public class IntegerParameter extends StringParameter {
     public final static String versionId =
-        "$Id: //core-platform/test-packaging/src/com/arsdigita/util/parameter/IntegerParameter.java#2 $" +
+        "$Id: //core-platform/test-packaging/src/com/arsdigita/util/parameter/IntegerParameter.java#3 $" +
         "$Author: justin $" +
-        "$DateTime: 2003/08/26 20:38:18 $";
+        "$DateTime: 2003/09/04 23:40:23 $";
 
     static {
         Converters.set(Integer.class, new IntegerConverter());
@@ -22,5 +22,11 @@ public class IntegerParameter extends StringParameter {
 
     public IntegerParameter(final String name) {
         super(name, Integer.class);
+    }
+
+    public IntegerParameter(final String name,
+                            final int multiplicity,
+                            final Object defaalt) {
+        super(name, multiplicity, defaalt, Integer.class);
     }
 }
