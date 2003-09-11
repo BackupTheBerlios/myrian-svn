6e8292835afec5df14bc255afc6fc8f02f379825
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-09-11 17:20:12 +0000

    @35903 	Filled out the implementation of parse.

--- archive/packaging/src/com/arsdigita/persistence/pdl/DirSource.java
+++ archive/packaging/src/com/arsdigita/persistence/pdl/DirSource.java
@@ -1,17 +1,20 @@
 package com.arsdigita.persistence.pdl;
 
+import com.arsdigita.util.*;
+
 import java.io.*;
+import java.util.*;
 
 /**
  * DirSource
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #1 $ $Date: 2003/09/11 $
+ * @version $Revision: #2 $ $Date: 2003/09/11 $
  **/
 
 public class DirSource implements PDLSource {
 
-    public final static String versionId = "$Id: //core-platform/test-packaging/src/com/arsdigita/persistence/pdl/DirSource.java#1 $ by $Author: rhs $, $DateTime: 2003/09/11 14:54:54 $";
+    public final static String versionId = "$Id: //core-platform/test-packaging/src/com/arsdigita/persistence/pdl/DirSource.java#2 $ by $Author: rhs $, $DateTime: 2003/09/11 17:20:12 $";
 
     private final File m_dir;
 
@@ -20,7 +23,47 @@ public class DirSource implements PDLSource {
     }
 
     public void parse(PDLCompiler compiler) {
-        // TODO: implement
+        parse(compiler, m_dir);
     }
+
+    private void parse(PDLCompiler compiler, File dir) {
+        File[] listing = dir.listFiles(new FileFilter() {
+            public boolean accept(File file) {
+                if (file.isDirectory()) {
+                    return true;
+                }
+
+                if (file.getName().endsWith(".pdl")) {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        Arrays.sort(listing, FILE_NAME);
+
+        for (int i = 0; i < listing.length; i++) {
+            File file = listing[i];
+            if (file.isDirectory()) {
+                parse(compiler, file);
+            } else {
+                try {
+                    compiler.parse
+                        (new FileReader(file), file.getAbsolutePath());
+                } catch (IOException e) {
+                    throw new UncheckedWrapperException(e);
+                }
+            }
+        }
+    }
+
+    private static final Comparator FILE_NAME = new Comparator() {
+        public int compare(Object o1, Object o2) {
+            File f1 = (File) o1;
+            File f2 = (File) o2;
+            return f1.getAbsolutePath().compareTo(f2.getAbsolutePath());
+        }
+    };
 
 }
