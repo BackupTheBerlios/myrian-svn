5ba7e69182e45a93302c900b4431931465b76630
jorris <jorris@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-10-08 12:24:17 +0000

    @36809 	   * Added PermissionDecorator to CMSSuite so that it runs with permissionsDML on 	   * Changed PermissionDecorator to use the admin user account instead of system party. 	     Workflow has some wacky logic for getting the user that finished a task (which it doesn't actually use), 	     that would result in a violation from the ACSObject instantiator trying to instantiate a Party as a User that 	     doesn't exist.

--- archive/core-platform/test/src/com/arsdigita/tools/junit/extensions/PermissionDecorator.java
+++ archive/core-platform/test/src/com/arsdigita/tools/junit/extensions/PermissionDecorator.java
@@ -18,13 +18,13 @@ package com.arsdigita.tools.junit.extensions;
 import junit.extensions.TestDecorator;
 import junit.framework.Test;
 import junit.framework.TestResult;
-import com.arsdigita.kernel.KernelExcursion;
-import com.arsdigita.kernel.Kernel;
+import com.arsdigita.kernel.*;
+import com.arsdigita.util.Assert;
 
 /**
  *
  * @author Jon Orris (jorris@redhat.com)
- * @version $Revision: #1 $ $DateTime: 2003/09/23 17:53:12 $
+ * @version $Revision: #2 $ $DateTime: 2003/10/08 12:24:17 $
  */
 public class PermissionDecorator extends TestDecorator {
     public PermissionDecorator(Test test) {
@@ -35,12 +35,27 @@ public class PermissionDecorator extends TestDecorator {
         final TestResult finalResult = testResult;
         KernelExcursion ex = new KernelExcursion() {
             protected void excurse() {
-                setParty(Kernel.getSystemParty());
+                setParty(getAdminUser());
                 PermissionDecorator.super.run(finalResult);
             }
         };
 
         ex.run();
+    }
+
+    private User getAdminUser() {
+        UserCollection uc = User.retrieveAll();
+
+        try {
+            uc.filter(KernelHelper.getSystemAdministratorEmailAddress());
+            uc.next();
+            User sysadmin = uc.getUser();
+            Assert.exists(sysadmin, User.class);
+
+            return sysadmin;
+        } finally {
+            uc.close();
+        }
     }
 
 }
