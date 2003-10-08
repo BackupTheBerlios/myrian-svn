c93bafa3a4ec2f7b5b8378f4983a82ce84fe91be
dan <dan@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-10-08 08:59:12 +0000

    @36787 	Library of useful routines for I/O.
    	   Currently contains one function:
    	
    	        static void sendFile(InputStream is, OutputStream os);
    	
    	   for doing a straight copy of data from one stream to other.

---
+++ archive/packaging/src/com/arsdigita/util/IO.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2001, 2002 Red Hat Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the CCM Public
+ * License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of
+ * the License at http://www.redhat.com/licenses/ccmpl.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ */
+
+
+package com.arsdigita.util;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+/**
+ * A library of methods for dealing with I/O streams.
+ */
+public class IO {
+    
+    /**
+     * Sends the contents of an input stream to another
+     * output stream.
+     *
+     * @param src the source file to be sent
+     * @param dst the destination to send the file to
+     */
+    public static void sendFile(InputStream src,
+                                OutputStream dst) 
+        throws IOException {
+        
+        byte buf[] = new byte[4096];
+        int ret;
+
+        while ((ret = src.read(buf)) != -1) {
+            dst.write(buf, 0, ret);
+        }
+    }
+    
+    
+    // XXX add a method that, given a InputStream
+    // figures out what character set the containing
+    // document is, ie reads the XML prolog.
+    // Also have similar char set discovery APIs
+    // for HttpServletRequest objects.
+}
