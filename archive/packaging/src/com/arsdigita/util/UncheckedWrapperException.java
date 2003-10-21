3bbe8b37434cb67183919274fb7088b506f7dfa0
dan <dan@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-10-21 10:56:46 +0000

    @37183 	Register an exception unwrapper

--- archive/packaging/src/com/arsdigita/util/UncheckedWrapperException.java
+++ archive/packaging/src/com/arsdigita/util/UncheckedWrapperException.java
@@ -17,6 +17,9 @@ package com.arsdigita.util;
 
 import org.apache.log4j.Logger;
 
+import com.arsdigita.logging.Exceptions;
+import com.arsdigita.logging.ExceptionUnwrapper;
+
 /**
  * A wrapper exception that can be used to rethrow another
  * exception.
@@ -34,11 +37,22 @@ import org.apache.log4j.Logger;
  * <tt>new UncheckedWrapperException(e.getMessage(), e);</tt>
  *
  * @author David Eison
- * @version $Id: //core-platform/test-packaging/src/com/arsdigita/util/UncheckedWrapperException.java#2 $
+ * @version $Id: //core-platform/test-packaging/src/com/arsdigita/util/UncheckedWrapperException.java#3 $
  */
 public class UncheckedWrapperException extends RuntimeException {
 
-    public static final String versionId = "$Id: //core-platform/test-packaging/src/com/arsdigita/util/UncheckedWrapperException.java#2 $";
+    public static final String versionId = "$Id: //core-platform/test-packaging/src/com/arsdigita/util/UncheckedWrapperException.java#3 $";
+
+    static {
+        Exceptions.registerUnwrapper(
+            UncheckedWrapperException.class,
+            new ExceptionUnwrapper() {
+                public Throwable unwrap(Throwable t) {
+                    UncheckedWrapperException ex = (UncheckedWrapperException)t;
+                    return ex.getRootCause();
+                }
+            });
+    }
 
     Throwable m_rootCause;
 
