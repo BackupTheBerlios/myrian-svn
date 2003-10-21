98c85288140d741342ba2b905a8ab19103e0869c
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-10-21 22:53:17 +0000

    @37236 	Made the validate method not NPE if the value is null.

--- archive/packaging/src/com/arsdigita/util/jdbc/JDBCURLParameter.java
+++ archive/packaging/src/com/arsdigita/util/jdbc/JDBCURLParameter.java
@@ -26,13 +26,13 @@ import org.apache.oro.text.perl.Perl5Util;
  * Subject to change.
  *
  * @author Justin Ross &lt;jross@redhat.com&gt;
- * @version $Id: //core-platform/test-packaging/src/com/arsdigita/util/jdbc/JDBCURLParameter.java#3 $
+ * @version $Id: //core-platform/test-packaging/src/com/arsdigita/util/jdbc/JDBCURLParameter.java#4 $
  */
 public class JDBCURLParameter extends StringParameter {
     public final static String versionId =
-        "$Id: //core-platform/test-packaging/src/com/arsdigita/util/jdbc/JDBCURLParameter.java#3 $" +
-        "$Author: justin $" +
-        "$DateTime: 2003/10/21 21:52:30 $";
+        "$Id: //core-platform/test-packaging/src/com/arsdigita/util/jdbc/JDBCURLParameter.java#4 $" +
+        "$Author: rhs $" +
+        "$DateTime: 2003/10/21 22:53:17 $";
 
     private static final Perl5Util s_perl = new Perl5Util();
     private static final String s_regex = "/^jdbc:[^:]+:.+$/";
@@ -52,7 +52,7 @@ public class JDBCURLParameter extends StringParameter {
 
         final String url = (String) value;
 
-        if (!s_perl.match(s_regex, url)) {
+        if (url != null && !s_perl.match(s_regex, url)) {
             final String message =
                 "The value must start with \"jdbc:\" and take the " +
                 "form jdbc:subprotocol:subname";
