9511ea5ef9b132a9869c8acfd250a0da98579f31
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-11-10 12:56:19 +0000

    @37872 	ALERT: Changed SQLLoader.java to do relative includes rather 	than absolute includes when a double @ symbol is used. This 	means that SQLLoader.java now matches the oracle include 	semantics for single and double @ includes. This is 	technically a bug fix, but theoretically it may cause some SQL 	scripts that previously worked with SQLLoader.java (but not 	with sqlplus) to break if they incorrectly use the double @ 	include syntax.

--- archive/core-platform/src/com/arsdigita/installer/SQLLoader.java
+++ archive/core-platform/src/com/arsdigita/installer/SQLLoader.java
@@ -12,12 +12,12 @@ import org.apache.log4j.Logger;
  * SQLLoader
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #2 $ $Date: 2003/10/28 $
+ * @version $Revision: #3 $ $Date: 2003/11/10 $
  **/
 
 public abstract class SQLLoader {
 
-    public final static String versionId = "$Id: //core-platform/dev/src/com/arsdigita/installer/SQLLoader.java#2 $ by $Author: jorris $, $DateTime: 2003/10/28 18:36:21 $";
+    public final static String versionId = "$Id: //core-platform/dev/src/com/arsdigita/installer/SQLLoader.java#3 $ by $Author: rhs $, $DateTime: 2003/11/10 12:56:19 $";
 
     private static final Logger s_log = Logger.getLogger(SQLLoader.class);
 
@@ -33,7 +33,7 @@ public abstract class SQLLoader {
         try {
             Statement stmt = m_conn.createStatement();
             try {
-                load(stmt, name, name);
+                load(stmt, name, null, name);
             } finally {
                 stmt.close();
             }
@@ -43,7 +43,7 @@ public abstract class SQLLoader {
     }
 
     private void load(final Statement stmt, final String base,
-                      final String name) {
+                      final String from, final String name) {
 	if (s_log.isInfoEnabled()) {
 	    s_log.info("Loading " + name + " using base " + base);
 	}
@@ -52,7 +52,8 @@ public abstract class SQLLoader {
             Reader reader = open(name);
             if (reader == null) {
                 throw new IllegalArgumentException
-                    ("no such file: " + name + ", included from: " + base);
+                    ("no such file: " + name +
+                     (from == null ? "" : (", included from: " + from)));
             }
 
             StatementParser sp = new StatementParser
@@ -61,8 +62,12 @@ public abstract class SQLLoader {
                      public void onStatement(String sql) {
                          execute(stmt, sql);
                      }
-                     public void onInclude(String include) {
-                         include(stmt, base, include);
+                     public void onInclude(String include, boolean relative) {
+                         if (relative) {
+                             include(stmt, base, name, include);
+                         } else {
+                             include(stmt, base, base, include);
+                         }
                      }
                  });
             sp.parse();
@@ -90,12 +95,13 @@ public abstract class SQLLoader {
         return path;
     }
 
-    private void include(Statement stmt, String base, String included) {
+    private void include(Statement stmt, String base, String from,
+                         String included) {
         if (s_log.isDebugEnabled()) {
             s_log.debug("Resolving include: '" + included + "'");
         }
 
-        String front = parent(base);
+        String front = parent(from);
         String back = included;
         while (back.startsWith("../")) {
             back = back.substring(3);
@@ -113,7 +119,7 @@ public abstract class SQLLoader {
             s_log.debug("Recursively including: '" + resolved + "'");
         }
 
-        load(stmt, base, resolved);
+        load(stmt, base, from, resolved);
     }
 
     private void execute(Statement stmt, String sql) {
