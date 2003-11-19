654fa5a8ada2d5f1901d258d0a5e1ff69c8b4473
jorris <jorris@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2003-11-19 13:31:41 +0000

    @38143 	Fix some errors running with Oracle. 	Fix DBExceptionHandlerTest to not hang under RHDB on AS 2.1

--- archive/core-platform/test/src/com/arsdigita/db/DbExceptionHandlerTest.java
+++ archive/core-platform/test/src/com/arsdigita/db/DbExceptionHandlerTest.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (C) 2001, 2002, 2003, 2003 Red Hat Inc. All Rights Reserved.
- *
- * The contents of this file are subject to the CCM Public
- * License (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of
- * the License at http://www.redhat.com/licenses/ccmpl.html
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- */
+* Copyright (C) 2001, 2002, 2003, 2003 Red Hat Inc. All Rights Reserved.
+*
+* The contents of this file are subject to the CCM Public
+* License (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of
+* the License at http://www.redhat.com/licenses/ccmpl.html
+*
+* Software distributed under the License is distributed on an "AS
+* IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* rights and limitations under the License.
+*
+*/
 
 package com.arsdigita.db;
 
@@ -20,6 +20,9 @@ import junit.extensions.*;
 
 import java.io.*;
 import java.sql.SQLException;
+import java.sql.Connection;
+
+import org.apache.log4j.Logger;
 
 /**
  * This test verifies the exception-type-narrowing functionality
@@ -29,71 +32,83 @@ import java.sql.SQLException;
  */
 public class DbExceptionHandlerTest extends TestCase {
 
-    public static final String versionId = "$Id: //core-platform/dev/test/src/com/arsdigita/db/DbExceptionHandlerTest.java#8 $";
+    public static final String versionId = "$Id: //core-platform/dev/test/src/com/arsdigita/db/DbExceptionHandlerTest.java#9 $";
 
-    private static java.sql.Connection conn;
+
 
     private static final String dirRoot =
-        System.getProperty("test.base.dir") +
-        "/com/arsdigita/db/";
+            System.getProperty("test.base.dir") +
+            "/com/arsdigita/db/";
     private static final String blobFileName = "adlogo.gif";
 
+    private static final Logger s_log = Logger.getLogger(DbExceptionHandlerTest.class);
     public DbExceptionHandlerTest(String name) {
         super(name);
     }
 
-    /**
-     * Technically, this code beyonds in a suite setup and teardown..
-     * Unfortunately, I can't quite figure out how to make that work
-     * right w/ the rest of our test architecture, so we'll stick them
-     * here for now.
-     */
-    public void setUp() throws SQLException {
-        conn = ConnectionManager.getConnection();
 
+    public void setUpTestTable(Connection conn) throws SQLException {
+
+        s_log.debug("got connection");
         java.sql.PreparedStatement tableStmt =
-            conn.prepareStatement(
-                                  "create table db_test (\n" +
-                                  "    theId          integer primary key\n" +
-                                  ")");
-
+                conn.prepareStatement(
+                        "create table db_test (\n" +
+                "    theId          integer primary key\n" +
+                ")");
+        s_log.debug("got statement");
         tableStmt.executeUpdate();
+        s_log.debug("got executed");
         tableStmt.close();
+        s_log.debug("got closed");
     }
 
-    public void tearDown() throws SQLException {
+    public void tearDownTestTable(Connection conn) throws SQLException {
         java.sql.PreparedStatement stmt;
+        s_log.debug("getting drop statement");
         stmt = conn.prepareStatement("drop table db_test");
+        s_log.debug("got drop statement");
         stmt.executeUpdate();
+        s_log.debug("executed update");
         stmt.close();
+        s_log.debug("closed");
     }
 
     public void testUniqueConstraintException() throws SQLException {
-        java.sql.PreparedStatement insertStmt =
-            conn.prepareStatement("insert into db_test\n" +
-                                  "(theId)\n" +
-                                  "values\n" +
-                                  "(1)");
-        insertStmt.executeUpdate();
+        s_log.debug("Start testUniqueConstraintException");
+        s_log.debug("getting connection");
+        Connection conn = ConnectionManager.getConnection();
+
+        java.sql.PreparedStatement insertStmt = null;
         try {
+            insertStmt =
+            conn.prepareStatement("insert into db_test\n" +
+            "(theId)\n" +
+            "values\n" +
+            "(1)");
             insertStmt.executeUpdate();
-            fail("Unique constraint violation should have caused error");        
+            fail("Unique constraint violation should have caused error");
         } catch (SQLException e) {
             SQLException wrapped = SQLExceptionHandler.wrap(e);
             if (wrapped instanceof UniqueConstraintException) {
                 //good
-            } else
-                // bad
+            } else {
+            // bad
                 fail("Unique constraint violation should have caused " +
-                     "UniqueConstraintException, instead caused " + e);        
+                        "UniqueConstraintException, instead caused " + e);
             }
-            insertStmt.close();
+        } finally {
+            if(insertStmt != null) {
+                insertStmt.close();
+            }
+            ConnectionManager.returnConnection(conn);
+        }
+        s_log.debug("End testUniqueConstraintException");
     }
 
     public void testDbNotAvailableException() throws SQLException {
         // TODO: Figure out other ways to simulate DB failure
         // than just garbage connection info?  Is this possible?
-
+        s_log.debug("Start testDbNotAvailableException");
         java.sql.PreparedStatement insertStmt = null;
         java.sql.Connection badConn = null;
         try {
@@ -109,7 +124,7 @@ public class DbExceptionHandlerTest extends TestCase {
                 // good
             } else {
                 throw new com.arsdigita.util.UncheckedWrapperException
-                    (wrapped);
+                        (wrapped);
                 /*
                 fail("Setting garbage connection info should have caused " +
                 "DbNotAvailableException, instead caused " + wrapped);*/
@@ -130,7 +145,7 @@ public class DbExceptionHandlerTest extends TestCase {
                 // good
             } else {
                 fail("Setting garbage connection info should have caused " +
-                     "DbNotAvailableException, instead caused " + wrapped);
+                        "DbNotAvailableException, instead caused " + wrapped);
             }
         }
 
@@ -147,17 +162,17 @@ public class DbExceptionHandlerTest extends TestCase {
                 // good
             } else {
                 fail("Setting garbage connection info should have caused " +
-                     "DbNotAvailableException, instead caused " + wrapped);
+                        "DbNotAvailableException, instead caused " + wrapped);
             }
         }
 
         try {
             badConn = java.sql.DriverManager.getConnection("jdbc:oracle:oci8:@totalgarbage");
             insertStmt =
-                badConn.prepareStatement("insert into db_test\n" +
-                                         "(theId)\n" +
-                                         "values\n" +
-                                         "(1)");
+                    badConn.prepareStatement("insert into db_test\n" +
+                    "(theId)\n" +
+                    "values\n" +
+                    "(1)");
             insertStmt.executeUpdate();
             fail("Using garbage connection info should have caused error");
         } catch (SQLException e) {
@@ -170,8 +185,10 @@ public class DbExceptionHandlerTest extends TestCase {
                 // good
             } else {
                 fail("Setting garbage connection info should have caused " +
-                     "DbNotAvailableException, instead caused " + wrapped);
+                        "DbNotAvailableException, instead caused " + wrapped);
             }
         }
+
+        s_log.debug("end testDbNotAvailableException");
     }
 }
