b91ec8dc6907cee7f4da14bb6510b495126cdc08
richardl <richardl@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2004-01-05 16:08:50 +0000

    @39114 	   ALERT: Fix BZ 111445. This also changes the unique constraint on the web_hosts 	   table from unique(server_name) to unique(server_name, server_port), enabling 	   multiple hosts with the same server name but different ports. Run the 	   included upgrade scripts on existing installations to update.

---
+++ archive/core-platform/sql/ccm-core/postgres/upgrade/6.0.0-6.0.1/update-host-unique-index.sql
@@ -0,0 +1,20 @@
+--
+-- Copyright (C) 2003, 2003 Red Hat Inc. All Rights Reserved.
+--
+-- The contents of this file are subject to the CCM Public
+-- License (the "License"); you may not use this file except in
+-- compliance with the License. You may obtain a copy of
+-- the License at http://www.redhat.com/licenses/ccmpl.html
+--
+-- Software distributed under the License is distributed on an "AS
+-- IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+-- implied. See the License for the specific language governing
+-- rights and limitations under the License.
+--
+-- $Id: //core-platform/dev/sql/ccm-core/postgres/upgrade/6.0.0-6.0.1/update-host-unique-index.sql#1 $
+-- $DateTime: 2004/01/05 16:08:50 $
+
+-- Drop old unique index on server name
+alter table web_hosts drop constraint web_hosts_server_name_u_frlsu;
+-- Add uniqueness on server name, server port
+alter table web_hosts add constraint web_hos_ser_nam_ser_po_u_las14 unique(server_name, server_port);
