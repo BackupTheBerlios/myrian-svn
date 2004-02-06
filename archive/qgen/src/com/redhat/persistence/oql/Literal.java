97a492a1f38f68db5dc6ff832cea890366fb6372
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2004-02-06 17:43:00 +0000

    @40162 	Tests and bug fixes for string literals.

--- archive/qgen/src/com/redhat/persistence/oql/Literal.java
+++ archive/qgen/src/com/redhat/persistence/oql/Literal.java
@@ -4,12 +4,12 @@ package com.redhat.persistence.oql;
  * Literal
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #2 $ $Date: 2004/02/06 $
+ * @version $Revision: #3 $ $Date: 2004/02/06 $
  **/
 
 public class Literal extends Expression {
 
-    public final static String versionId = "$Id: //core-platform/test-qgen/src/com/redhat/persistence/oql/Literal.java#2 $ by $Author: rhs $, $DateTime: 2004/02/06 15:43:04 $";
+    public final static String versionId = "$Id: //core-platform/test-qgen/src/com/redhat/persistence/oql/Literal.java#3 $ by $Author: rhs $, $DateTime: 2004/02/06 17:43:00 $";
 
     private Object m_value;
 
@@ -24,10 +24,35 @@ public class Literal extends Expression {
     Code.Frame frame(Code code) {
         Code.Frame frame = code.frame(code.getType(m_value));
         // XXX: should have option to use bind variables here
-        frame.setColumns(new String[] { m_value.toString() });
+        String literal;
+        if (m_value instanceof String) {
+            literal = quote((String) m_value);
+        } else {
+            literal = m_value.toString();
+        }
+        frame.setColumns(new String[] { literal });
         code.addVirtual(this);
         code.setFrame(this, frame);
         return frame;
+    }
+
+    // XXX: temporary hack
+    private static String quote(String value) {
+        StringBuffer result = new StringBuffer(2*value.length());
+        result.append("'");
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            switch (c) {
+            case '\'':
+                result.append("'");
+                break;
+            default:
+                result.append(c);
+                break;
+            }
+        }
+        result.append("'");
+        return result.toString();
     }
 
     void emit(Code code) {
