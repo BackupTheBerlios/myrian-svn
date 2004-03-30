3a3826269db5d5309fdb5cfcec37f65d2938e651
dennis <dennis@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2004-03-30 17:42:55 +0000

    @41911 	manually fixing the copyright notice

--- archive/core-platform/sql/ccm-core/default/formbuilder/comment-forms_unique_id_seq.sql
+++ archive/core-platform/sql/ccm-core/default/formbuilder/comment-forms_unique_id_seq.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright (C) 2002-2004 Red Hat Inc. All Rights Reserved.
+--
+-- The contents of this file are subject to the Open Software License v2.1
+-- (the "License"); you may not use this file except in compliance with the
+-- License. You may obtain a copy of the License at
+-- http://rhea.redhat.com/licenses/osl2.1.html.
+--
+-- Software distributed under the License is distributed on an "AS
+-- IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+-- implied. See the License for the specific language governing
+-- rights and limitations under the License.
+--
+-- $Id: //core-platform/dev/sql/ccm-core/default/formbuilder/comment-forms_unique_id_seq.sql#2 $
+-- $DateTime: 2004/03/30 17:42:55 $
+
 --comment on sequence forms_unique_id_seq is '
 --  Sequence for generating unique form submission ID
 --  numbers
