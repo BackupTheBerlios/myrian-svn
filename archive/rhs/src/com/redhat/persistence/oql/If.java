f9a693b3bce28f32db2072ee4be676a5ec0b16f0
rhs <rhs@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2004-05-05 19:30:44 +0000

    @42470 	Added support for if expressions in oql.

---
+++ archive/rhs/src/com/redhat/persistence/oql/If.java
@@ -0,0 +1,71 @@
+package com.redhat.persistence.oql;
+
+import com.redhat.persistence.metadata.*;
+
+/**
+ * If
+ *
+ * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
+ * @version $Revision: #1 $ $Date: 2004/05/05 $
+ **/
+
+public class If extends Expression {
+
+    public final static String versionId = "$Id: //users/rhs/persistence/src/com/redhat/persistence/oql/If.java#1 $ by $Author: rhs $, $DateTime: 2004/05/05 19:30:44 $";
+
+    private Expression m_condition;
+    private Expression m_consequence;
+    private Expression m_alternative;
+
+    public If(Expression condition, Expression consequence,
+              Expression alternative) {
+        m_condition = condition;
+        m_consequence = consequence;
+        m_alternative = alternative;
+    }
+
+    void hash(Generator gen) {
+        m_condition.hash(gen);
+        m_consequence.hash(gen);
+        m_alternative.hash(gen);
+        gen.hash(getClass());
+    }
+
+    void frame(Generator gen) {
+        m_condition.frame(gen);
+        m_consequence.frame(gen);
+        m_alternative.frame(gen);
+        QFrame cframe = gen.getFrame(m_consequence);
+        QFrame aframe = gen.getFrame(m_alternative);
+        QFrame frame = gen.frame
+            (this, merge(cframe.getType(), aframe.getType()));
+    }
+
+    private ObjectType merge(ObjectType a, ObjectType b) {
+        if (a == null) { return b; }
+        if (b == null) { return a; }
+        if (a.isSubtypeOf(b)) {
+            return b;
+        }
+        if (b.isSubtypeOf(a)) {
+            return a;
+        }
+        throw new IllegalArgumentException
+            ("can't merge types: " + a + ", " + b);
+    }
+
+    Code emit(Generator gen) {
+        return new Code("case when ")
+            .add(m_condition.emit(gen))
+            .add(" then ")
+            .add(m_consequence.emit(gen))
+            .add(" else ")
+            .add(m_alternative.emit(gen))
+            .add(" end");
+    }
+
+    String summary() {
+        return "if";
+    }
+
+}
