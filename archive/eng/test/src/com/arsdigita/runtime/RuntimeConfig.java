b0e9a2e8eeb04bd9b55f17e60dfd3a0a5d7cd2ca
vadim <vadim@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2004-06-17 11:58:17 +0000

    @43233 	Provide a way to customize the JDBC URL.

--- archive/eng/test/src/com/arsdigita/runtime/RuntimeConfig.java
+++ archive/eng/test/src/com/arsdigita/runtime/RuntimeConfig.java
@@ -1,15 +1,21 @@
 package com.arsdigita.runtime;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * RuntimeConfig
  *
  * @author Rafael H. Schloming &lt;rhs@mit.edu&gt;
- * @version $Revision: #1 $ $Date: 2004/06/07 $
+ * @version $Revision: #2 $ $Date: 2004/06/17 $
  **/
 
 public class RuntimeConfig {
+    private final static String PROPERTIES = "rhs.properties";
+    private final static String JDBC_URL = "jdbc.url";
 
-    public final static String versionId = "$Id: //eng/persistence/dev/test/src/com/arsdigita/runtime/RuntimeConfig.java#1 $ by $Author: rhs $, $DateTime: 2004/06/07 13:49:55 $";
+    public final static String versionId = "$Id: //eng/persistence/dev/test/src/com/arsdigita/runtime/RuntimeConfig.java#2 $ by $Author: vadim $, $DateTime: 2004/06/17 11:58:17 $";
 
     private static final RuntimeConfig CONFIG = new RuntimeConfig();
 
@@ -18,7 +24,24 @@ public class RuntimeConfig {
     }
 
     public String getJDBCURL() {
-        return "jdbc:postgresql:rafaels?user=rafaels";
-    }
+        InputStream is =
+            getClass().getClassLoader().getResourceAsStream(PROPERTIES);
 
+        if (is == null) {
+            throw new IllegalStateException
+                ("Couldn' find " + PROPERTIES + " anywhere on the classpath.");
+        }
+
+        Properties props = new Properties();
+        try {
+            props.load(is);
+        } catch (IOException ex) {
+            RuntimeException rex =
+                new IllegalStateException("can't load " + PROPERTIES);
+            rex.initCause(ex);
+            throw rex;
+        }
+        return props.getProperty(JDBC_URL,
+                                 "jdbc:postgresql:rafaels?user=rafaels");
+    }
 }
