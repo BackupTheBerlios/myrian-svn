2fba36374dc47adef18c36d07b36519578ec95c2
vadim <vadim@0c4ed275-74e5-0310-b0c6-b6ea092b5e81>
2004-06-25 13:51:17 +0000

    @43404 	checkpointing current formatting changes

---
+++ archive/eng/sql/postgresql/drop.sql
@@ -0,0 +1,20 @@
+-- Since: 2004-06-25
+-- Temporary hack to clean up the schema
+
+drop table items;
+drop table orders;
+drop table products;
+drop table pictures;
+drop table group_member_map;
+drop table groups;
+drop table users;
+drop table auxiliary_emails;
+drop table parties;
+
+drop table emps;
+drop table depts;
+
+
+
+
+
